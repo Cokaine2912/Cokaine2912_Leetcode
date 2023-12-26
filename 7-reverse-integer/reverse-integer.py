a624c3c2cb98290b4088136f94fe22d6032b7147
Niraj Prashant Kamble <niraj28352@gmail.com>
2023-12-26 22:18:09 +0530

    Time: 42 ms (41.76%) | Memory: 17.3 MB (5.62%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        ans = []
+        num = abs(x)
+        while num > 0 :
+            ans.append(num % 10)
+            num = (num - num%10)//10
+        result = 0
+        n = len(ans)
+        for i in range(n-1,-1,-1) :
+            result += ans[i]*10**(n-i-1)
+        if result > 2**31 - 1 :
+            return 0
+        return result if x > 0 else -1*result
+
+
+        
